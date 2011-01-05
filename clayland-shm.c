0f88bcf4499c7f9df9146eec3aaa32b55e358c3e
nobled <nobled@dreamwidth.org>
2011-01-05 19:22:21 +0000

    add shm buffer interface
    Hopefully clients can actually do something now.

---
+++ clayland-shm.c
@@ -0,0 +1,122 @@
+
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "clayland.h"
+
+#define CLAYLAND_TYPE_SHM_BUFFER            (clayland_shm_buffer_get_type ())
+#define CLAYLAND_SHM_BUFFER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), CLAYLAND_TYPE_SHM_BUFFER, ClaylandShmBuffer))
+#define CLAYLAND_SHM_BUFFER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), CLAYLAND_TYPE_SHM_BUFFER, ClaylandShmBufferClass))
+#define CLAYLAND_IS_SHM_BUFFER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CLAYLAND_TYPE_SHM_BUFFER))
+#define CLAYLAND_IS_SHM_BUFFER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), CLAYLAND_TYPE_SHM_BUFFER))
+#define CLAYLAND_SHM_BUFFER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), CLAYLAND_TYPE_SHM_BUFFER, ClaylandShmBufferClass))
+
+typedef struct _ClaylandShmBuffer ClaylandShmBuffer;
+typedef struct _ClaylandShmBufferClass ClaylandShmBufferClass;
+
+struct _ClaylandShmBuffer {
+	ClaylandBuffer		 cbuffer;
+	guint8_t		*data;
+	size_t			 size;
+};
+
+struct _ClaylandShmBufferClass {
+	ClaylandBufferClass	 cbuffer_class;
+};
+
+G_DEFINE_TYPE (ClaylandShmBuffer, clayland_shm_buffer, CLAYLAND_TYPE_BUFFER);
+
+static void
+clayland_shm_buffer_class_init (ClaylandShmBufferClass *klass)
+{
+}
+
+static void
+clayland_shm_buffer_init (ClaylandShmBuffer *buffer)
+{
+}
+
+static void
+shm_buffer_destroy(struct wl_resource *resource, struct wl_client *client)
+{
+	ClaylandShmBuffer *buffer =
+		container_of(resource, ClaylandShmBuffer, cbuffer.buffer.resource);
+
+	munmap(buffer->data, buffer->size);
+	buffer->data = NULL;
+	g_object_unref(buffer);
+}
+
+static void
+client_shm_destroy_buffer(struct wl_client *client, struct wl_buffer *buffer)
+{
+	ClaylandBuffer *buffer =
+		container_of(resource, ClaylandBuffer, buffer.resource);
+
+	/* XXX: Is this redundant with the resource destroy callback? */
+}
+
+static const struct wl_buffer_interface shm_buffer_interface = {
+	client_shm_destroy_buffer
+};
+
+static void
+shm_buffer_create(struct wl_client *client, struct wl_shm *shm,
+		  uint32_t id, int fd, int32_t width, int32_t height,
+		  uint32_t stride, struct wl_visual *visual)
+{
+	ClaylandCompositor *compositor =
+	    container_of((struct wl_object *)shm, ClaylandCompositor, shm_object);
+	ClaylandShmBuffer *buffer;
+	CoglPixelFormat pformat;
+	CoglTextureFlags flags = COGL_TEXTURE_NONE; /* XXX: tweak flags? */
+
+	buffer = g_object_new(CLAYLAND_TYPE_SHM_BUFFER, NULL);
+	if (buffer == NULL) {
+		wl_client_post_no_memory(client);
+		return;
+	}
+
+	pformat = _clayland_init_buffer(&buffer->cbuffer, compositor,
+	                                id, width, height, visual);
+	if (pformat == COGL_PIXEL_FORMAT_ANY) {
+		/* XXX: report error? */
+		g_object_unref(buffer);
+		return;
+	}
+
+	/* override the default Clayland implementation */
+	buffer->cbuffer.buffer.resource.destroy = shm_buffer_destroy;
+/* XXX: May want to override this, too, once it does anything. */ 
+/*	buffer->cbuffer.buffer.resource.object.implementation =
+	    (void (**)(void)) &shm_buffer_interface;
+*/
+
+	buffer->size = stride * height;
+	buffer->data = mmap(NULL, buffer->size,
+	                    PROT_READ, MAP_SHARED, fd, 0);
+	(void) close(fd);
+	if (buffer->data == MAP_FAILED) {
+		g_object_unref(buffer);
+		return;
+	}
+
+	buffer->cbuffer.tex_handle =
+	cogl_texture_new_from_data((unsigned int)width, (unsigned int)height,
+	                flags, pformat, COGL_PIXEL_FORMAT_ANY, buffer->data);
+
+	if (buffer->cbuffer.tex_handle == COGL_INVALID_HANDLE) {
+		/* XXX: move munmap into GObject destructor? */
+		munmap(buffer->data, buffer->size);
+		g_object_unref(buffer);
+		return;
+	}
+
+
+	wl_client_add_resource(client, &buffer->cbuffer.buffer.resource);
+}
+
+const struct wl_shm_interface clayland_shm_interface = {
+	shm_buffer_create
+};
+
